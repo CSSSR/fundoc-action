acead6270c1dd86f3c460c3fb2c74895bfbfda17
Sergey Golovin <golovim@gmail.com>
2021-01-25 14:54:35 +0300

    update base Docker image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.10
+FROM rust:1.31
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-curl -LSfs https://japaric.github.io/trust/install.sh | \
-    sh -s -- --git daynin/fundoc
+cargo install fundoc
 fundoc
 
